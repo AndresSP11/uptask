41273e94ee8c5f28867204abf83290106e9a7bb4
Andres <andres.salazar.p@uni.pe>
2023-12-08 15:09:49 -0500

    Completanado la parte del Controller

--- UpTask_MVC/public/index.php
+++ UpTask_MVC/public/index.php
@@ -1,12 +1,43 @@
 <?php 
+/* Lugar donde definiremos las rutas */
+
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
+//Iniciar Sesión
+$router->get('/',[LoginController::class,'login']);
+$router->post('/',[LoginController::class,'login']);
+
+//Cerrar Sesión
+
+$router->get('/logout',[LoginController::class,'logout']);
+
+//Crear Cuenta
+
+$router->get('/crear',[LoginController::class,'crear']);
+$router->get('/crear',[LoginController::class,'crear']);
+
+//Olvide Password, donde se introducirá el correo.
+
+$router->get('/olvide',[LoginController::class,'olvide']);
+$router->post('/olvide',[LoginController::class,'olvide']);
+
+//Introduciendo la nueva contraseña.
+
+$router->get('/reestablecer',[LoginController::class,'reestablecer']);
+$router->post('/reestablecer',[LoginController::class,'reestablecer']);
+
+//Confirmación de Cuenta
+
+$router->get('/mensaje',[LoginController::class,'mensaje']);
+$router->post('/confirmar',[LoginController::class,'confirmar']);
 
 
+//Formulario de Olvide mi password
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
