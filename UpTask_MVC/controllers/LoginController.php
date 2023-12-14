09296be1201cfa28c65b4bebaf46a5c082d32620
Andres <andres.salazar.p@uni.pe>
2023-12-14 15:40:09 -0500

    Agregando la parte del trabjo

--- UpTask_MVC/controllers/LoginController.php
+++ UpTask_MVC/controllers/LoginController.php
@@ -1,24 +1,39 @@
 <?php
+/* En este caso, se verá las variables */
 
 namespace Controllers;
 
-class LoginController{
-    public static function login(){
-        echo "Desde login"; 
-        if($_SERVER['REQUEST_METHOD']==='POST'){
+use MVC\Router;
 
+class LoginController{
+    public static function login(Router $router){
+        if($_SERVER['REQUEST_METHOD']==='POST'){
+            
         }
+    $router->render('auth/login',[
+        'titulo'=>'Iniciar Sesión'
+    ]);
     }
+    
+
     public static function logout(){
         /* Metodo get suficiente para cerrar sesión */
         echo "Desde logout"; 
     }
-    public static function crear(){
-        echo "Desde crear"; 
+
+
+
+    public static function crear(Router $router){
+        
         if($_SERVER['REQUEST_METHOD']==='POST'){
 
         }
+    $router->render('auth/crear',[
+        'titulo'=>'Crear'
+    ]);
     }
+
+
     public static function olvide(){
         echo "Desde olvide"; 
         if($_SERVER['REQUEST_METHOD']==='POST'){
